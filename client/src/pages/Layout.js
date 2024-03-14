c1ae35b0d21d818c11977d1d5f896c8327509d8a
Fbombi <filippo.bomben@hotmail.it>
2024-03-14 21:23:20 +0100

    abbellimento menu

--- client/src/pages/Layout.js
+++ client/src/pages/Layout.js
@@ -1,9 +1,10 @@
 import { useEffect } from "react";
-import { Outlet, Link, useNavigate } from "react-router-dom";
+import { Outlet, Link, useNavigate, useLocation } from "react-router-dom";
 import logo from "../assets/images/logo.png";
 
 const Layout = () => {
     const usenavigate = useNavigate();
+    const location =useLocation();
 
     useEffect(() => {
         let username = sessionStorage.getItem("username");
@@ -11,39 +12,39 @@ const Layout = () => {
             usenavigate("/login");
         }
     });
-
+    
     return (
         <>
             <div className="p-4 bg-white dark:bg-gray-950 h-screen space-y-4 flex flex-col">
                 <header
                     data-testid="header"
-                    className="bg-gray-200 dark:bg-gray-900 rounded-3xl shadow-lg flex justify-center items-center"
+                    className="bg-gray-200 dark:bg-gray-900 rounded-3xl shadow-lg flex justify-between items-center"
                 >
-                    <nav>
-                        <ul>
-                            <li className="float-left">
-                                <Link to="/" className="py-14 px-16">
+                    <nav className="flex">
+                        <ul className="flex">
+                            <li className="ml-12">
+                                <Link to="/" className={`text-lg py-3 px-3 mr-5 text-black font-bold ${location.pathname === '/' ? 'no-underline' : 'cursor-pointer underline decoration-[3px] underline-offset-4 hover:no-underline'}`}  >
                                     Home
                                 </Link>
                             </li>
                             <li className="float-left">
-                                <Link to="/profilo" className="py-14 px-16">
+                                <Link to="/profilo" className={`text-lg py-3 px-3 mr-5 text-black font-bold ${location.pathname === '/profilo' ? 'no-underline' : 'cursor-pointer underline decoration-[3px] underline-offset-4 hover:no-underline'}`}>
                                     Profilo
                                 </Link>
                             </li>
                             <li className="float-left">
-                                <Link to="/catprod" className="py-14 px-16">
+                                <Link to="/catprod" className={`text-lg py-3 px-3 mr-5 text-black font-bold ${location.pathname === '/catprod' ? 'no-underline' : 'cursor-pointer underline decoration-[3px] underline-offset-4 hover:no-underline'}`}>
                                     Catalogo Prodotti
                                 </Link>
                             </li>
                             <li className="float-left">
-                                <Link to="/catcli" className="py-14 px-16">
+                                <Link to="/catcli" className={`text-lg py-3 px-3 text-black font-bold ${location.pathname === '/catcli' ? 'no-underline' : 'cursor-pointer underline decoration-[3px] underline-offset-4 hover:no-underline'}`}>
                                     Catalogo Clienti
                                 </Link>
                             </li>
                         </ul>
                     </nav>
-                    <div>
+                    <div className="mr-12">
                         <img src={logo} alt="logo"></img>
                     </div>
                 </header>
