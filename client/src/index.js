70e679091a0130b3084e61e0e3062e7fc3a37f9f
Ciro9320 <ciropassata93@gmail.com>
2024-03-27 10:16:21 +0100

    Corretto package.json del client

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "primeicons/primeicons.css";
 import { PrimeReactProvider } from "primereact/api";
 import Tailwind from "primereact/passthrough/tailwind";
