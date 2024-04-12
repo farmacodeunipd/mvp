8d2ae66f16725d3737a5e4696e96fb19ec380123
rossonlorenzo <rossonlorenzo@gmail.com>
2024-04-12 16:08:09 +0200

    fixed-algo test version-minor

--- algo/tests/test_algo.py
+++ algo/tests/test_algo.py
@@ -1,13 +1,13 @@
 import pytest
 import requests
 import os
+import threading
 
 from app import nn_model
 from app import svd_model
 from app import nn_operator
 from app import svd_operator
 from app import model_context
-from app import ModelContext
 
 def testSVD_top5_1UserNItem():
     user = 120
@@ -42,7 +42,40 @@ def api_url():
     hostname = os.getenv("ALGO_URL", "localhost")
     return "http://" + hostname + ":4000"
 
-def test_api_response_svd(api_url):
+def test_train_api_response_svd(api_url):
+    response = requests.get(api_url + "/train/SVD")
+
+    assert response.status_code == 200
+    assert response.json()['message'] == 'Training successful'
+    assert response.json()['algo'] == 'SVD'
+    
+def test_train_api_response_nn(api_url):
+    response = requests.get(api_url + "/train/NN")
+
+    assert response.status_code == 200
+    assert response.json()['message'] == 'Training successful'
+    assert response.json()['algo'] == 'NN'
+    
+def test_train_api_response_already_training(api_url):
+    def make_training_request():
+        return requests.get(api_url + "/train/SVD")
+    
+    def make_second_training_request():
+        response = make_training_request()
+        assert response.status_code == 200
+        assert response.json()['message'] == 'Training in progress. Please wait a few minutes and try again later.'
+
+    thread1 = threading.Thread(target=make_training_request)
+    
+    thread2 = threading.Thread(target=make_second_training_request)
+
+    thread1.start()
+    thread2.start()
+    
+    thread2.join()
+    thread1.join()
+    
+def test_search_api_response_svd_user(api_url):
     response = requests.get(api_url + "/search/SVD/user/120/5")
 
     assert response.status_code == 200
@@ -55,9 +88,35 @@ def test_api_response_svd(api_url):
         assert "id" in item and isinstance(item["id"], str)
         assert "value" in item and isinstance(item["value"], int)
         
+def test_search_api_response_svd_item(api_url):
+    response = requests.get(api_url + "/search/SVD/item/1101100/5")
+
+    assert response.status_code == 200
+
+    response_data = response.json()
+    assert isinstance(response_data, list)
+
+    for item in response_data:
+        assert isinstance(item, dict)
+        assert "id" in item and isinstance(item["id"], str)
+        assert "value" in item and isinstance(item["value"], int)
         
-def test_api_response_nn(api_url):
+        
+def test_search_api_response_nn_user(api_url):
     response = requests.get(api_url + "/search/NN/user/120/5")
+
+    assert response.status_code == 200
+
+    response_data = response.json()
+    assert isinstance(response_data, list)
+
+    for item in response_data:
+        assert isinstance(item, dict)
+        assert "id" in item and isinstance(item["id"], str)
+        assert "value" in item and isinstance(item["value"], int)
+        
+def test_search_api_response_nn_item(api_url):
+    response = requests.get(api_url + "/search/NN/item/1101100/5")
 
     assert response.status_code == 200
 
